92ceafe321a1365536a9cb2771b75117151a61f7
Matt Campbell <cmampbell@gmail.com>
2023-01-28 15:12:49 -0500

    implemented user logout

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from sqlalchemy.exc import IntegrityError
 
 from forms import UserAddForm, LoginForm, MessageForm
 from models import db, connect_db, User, Message
+import pdb
 
 CURR_USER_KEY = "curr_user"
 
@@ -23,6 +24,8 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
 toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
+
+app.app_context().push()
 
 
 ##############################################################################
@@ -113,7 +116,9 @@ def login():
 def logout():
     """Handle logout of user."""
 
-    # IMPLEMENT THIS
+    do_logout()
+    flash("Successfully logged out!")
+    return redirect('/')
 
 
 ##############################################################################
